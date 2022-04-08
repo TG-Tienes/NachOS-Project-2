74d5251a50b514f8b0ab6b4a71f0ed19c6f88698
Nguyen Van Viet <Nvviet17.4@gmail.com>
2022-04-08 16:20:53 +0700

    add close file (ten la read nhung ma thuc ra la close:)))

---
+++ Source/nachos-3.4/code/test/readFile.c
@@ -0,0 +1,22 @@
+#include "syscall.h"
+
+int main()
+{
+    char *fileName;
+    int createFile = 1;
+    int id;
+
+    PrintString("Enter file name to read: ");
+    ReadString(fileName);
+    id = Open(fileName);
+    if ( id == -1) {
+        PrintString("Error can not open file: ");
+        PrintString(fileName);
+    }
+    else
+        Read(fileName, 256, id);
+    
+    Close(id);
+
+    Halt();
+}
