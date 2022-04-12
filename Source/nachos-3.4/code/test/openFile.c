13c8a39b7c7e5af39f24c6cdab4a96da3e88e532
Ihn_20157 <82387241+HelloWorld-00001@users.noreply.github.com>
2022-04-12 21:15:10 +0700

    fix bug openFile.c

--- Source/nachos-3.4/code/test/openFile.c
+++ Source/nachos-3.4/code/test/openFile.c
@@ -1,11 +1,17 @@
 #include "syscall.h"
 
+
+
 void openFile()
+
 {
+
     char *fileName;
     int createFile;
+    
     PrintString("Enter file name to Open: ");
     ReadString(fileName);
+
     createFile = Open(fileName);
     if (createFile == -1) {
         PrintString("Error can not open file: ");
@@ -16,19 +22,23 @@ void openFile()
     }
     else {
         PrintString("Open succeeded\n");
-        PrintString("Num of file open: ");
-        PrintNum(createFile);
     }
+
 }
 
 int main()
+
 {
-    int n = 1;
-    while ( n != 0) {
+
+    char n = '1';
+    while ( n != '0') {
         openFile();
         PrintString("Do you want to open more\nEnter 1 to open, 0 to close\n ");
-        ReadNum(n);
+        n = ReadChar();
     }
+
     
+
     Halt();
+
 }
