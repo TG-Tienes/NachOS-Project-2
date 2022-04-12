c5e7f7310e230cf98802895daf9928c96a5dda20
Ihn_20157 <82387241+HelloWorld-00001@users.noreply.github.com>
2022-04-12 21:05:06 +0700

    Update copy.c
    fix some bug of copy program

--- Source/nachos-3.4/code/test/copy.c
+++ Source/nachos-3.4/code/test/copy.c
@@ -1,29 +1,36 @@
 #include "syscall.h"
 
+
+
 int main()
+
 {
-    int id1, id2, size1;
+
+    int id1, id2, size1, size2;
     float percent;
     int count = 0;
     char* fileName1, *fileName2, *inf;
+
     PrintString("\nPlease enter your origin file name: ");
     ReadString(fileName1);
+    id1 = Open(fileName1);
+    size1 = FileSize(id1);
+
     PrintString("\nPlease enter your file name to copy: ");
     ReadString(fileName2);
-    id1 = Open(fileName1);
     id2 = Open(fileName2);
+    
     if ( id1 == -1 || id2 == -1 ) {
         PrintString("\n!!! ERROR !!! Can not find this file\n\n");
     } else {
-        size1 = FileSize(id1);
         while ( count <= size1) {
-            Read(inf, 64, id1 );
+            Read(inf, 64, id1);
             Write(inf, 64, id2);
-            percent = count / size1 * 100;
-            PrintString("\nCopy ... ");
-            PrintNum(percent);
-            count += 64;
+	        count += 64;
         }
+	    PrintString("\nCopy susscessful\n");
     }
+
     Halt();
+
 }
